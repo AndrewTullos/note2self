60ebdbc7d423a3a349db87fb14cca76391b20592
Drew Tullos <tullos.andrew@gmail.com>
2023-11-23 13:37:07 -0600

    home and notes fix

--- server.js
+++ server.js
@@ -1,42 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
-// const { clog } = require('./middleware/clog');
 const api = require("./routes/index.js");
-const port = 3000;
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
-// Import custom middleware, "cLog"
-app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
+// Serve static files from the public directory
 app.use(express.static("public"));
 
 // GET Route for homepage
-app.get("/", (req, res) =>
-	res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // GET Route for notes page
 app.get("/notes", (req, res) => {
-	res.redirect("/notes.html");
+	res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// GET Route for feedback page
-// app.get('/feedback', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-// );
-
-// Wildcard route to direct users to a 404 page
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-// );
-
-app.listen(PORT, () => console.log(`App listening at localhost:${PORT}`));
+// Start the server
+app.listen(PORT, () =>
+	console.log(`App listening at http://localhost:${PORT}`)
+);
